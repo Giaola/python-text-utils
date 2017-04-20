fa7e949b6fe9d469127fc08aabf3d9228830cc3a
Marsel Tzatzo <tmarselg@gmail.com>
2017-04-20 11:41:20 +0300

    Test punctuation when no punctuation

--- python_text_utils/tests/test_generic.py
+++ python_text_utils/tests/test_generic.py
@@ -12,6 +12,11 @@ class TestGeneric(unittest.TestCase):
         self.assertEqual(Generic.strip_punctuation_unicode(with_punctuation),
                          without_punctuation)
 
+    def test_strip_punctuation_unicode_no_punctuation(self):
+        without_punctuation = 'this'
+        self.assertEqual(Generic.strip_punctuation_unicode(without_punctuation),
+                         without_punctuation)
+
     def test_to_unicode(self):
         non_unicode = 'unicode'
         unicode = u'unicode'
